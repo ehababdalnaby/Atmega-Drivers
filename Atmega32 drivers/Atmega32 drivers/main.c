99173823275ba04eef973096481027e45d0c775a
ehababdalnaby <89157664+ehababdalnaby@users.noreply.github.com>
2021-08-18 21:34:32 +0200

    Add files via upload

---
+++ Atmega32 drivers/Atmega32 drivers/main.c
@@ -0,0 +1,18 @@
+/*
+ * Atmega32 drivers.c
+ *
+ * Created: 8/18/2021 9:30:05 PM
+ * Author : ehab2_phjirwi
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
