6e4408888360c989ab0ed6be7fd9c49b7ec73aad
ehababdalnaby <ehab_abdalnaby@outlook.com>
2021-08-19 14:02:46 +0200

    data types

--- Atmega32 drivers/Atmega32 drivers/Utility/dataTypes.h
+++ Atmega32 drivers/Atmega32 drivers/Utility/dataTypes.h
@@ -8,10 +8,16 @@
 
 #ifndef DATATYPES_H_
 #define DATATYPES_H_
+
 typedef unsigned char u8;
 typedef signed char s8;
-typedef unsigned int u32;
-typedef signed int s32;
+typedef unsigned int u16;
+typedef signed int s16;
+typedef unsigned long u32;
+typedef signed long s32;
+
+
+
 
 
 #endif /* DATATYPES_H_ */
