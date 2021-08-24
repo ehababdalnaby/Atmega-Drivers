cfcc25b9174962c85582b7360745243ae3526920
muhamedebrahim50 <75540684+muhamedebrahim50@users.noreply.github.com>
2021-08-24 22:28:04 +0200

    a

---
+++ Atmega32 drivers/Atmega32 drivers/HAL/inc/UARTgps.h
@@ -0,0 +1,16 @@
+/*
+ * UARTgps.h
+ *
+ * Created: 8/24/2021 10:09:11 PM
+ *  Author: Mohamed
+ */ 
+
+
+#ifndef UARTGPS_H_
+#define UARTGPS_H_
+#include "includes.h"
+
+
+
+
+#endif /* UARTGPS_H_ */
---
+++ Atmega32 drivers/Atmega32 drivers/HAL/src/UARTgps.c
@@ -0,0 +1,6 @@
+/*
+ * UARTgps.c
+ *
+ * Created: 8/24/2021 10:08:21 PM
+ *  Author: Mohamed
+ */ 
