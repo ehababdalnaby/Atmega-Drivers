d0201402d8eb749f47d5886baa36e95610efe0a2
mmxa1 <89199209+mmxa1@users.noreply.github.com>
2021-08-26 08:39:44 +0200

    I2C Start

---
+++ Atmega32 drivers/Atmega32 drivers/MCAL/inc/I2C.h
@@ -0,0 +1,16 @@
+/*
+ * I2C.h
+ *
+ * Created: 8/25/2021 5:55:12 PM
+ *  Author: Mahmoud-PC
+ */ 
+
+
+#ifndef I2C_H_
+#define I2C_H_
+
+
+
+
+
+#endif /* I2C_H_ */
---
+++ Atmega32 drivers/Atmega32 drivers/MCAL/src/I2C.c
@@ -0,0 +1,6 @@
+/*
+ * I2C.c
+ *
+ * Created: 8/25/2021 5:55:26 PM
+ *  Author: Mahmoud-PC
+ */ 
