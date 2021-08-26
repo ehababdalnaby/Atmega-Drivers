136f8348ea21ca4388f3c74ef05e29b06e49b400
ehababdalnaby <ehab_abdalnaby@outlook.com>
2021-08-26 22:29:21 +0200

    comments

--- Atmega32 drivers/Atmega32 drivers/HAL/src/RTC.c
+++ Atmega32 drivers/Atmega32 drivers/HAL/src/RTC.c
@@ -38,7 +38,7 @@ void Read_RTC(u8* Time,u8* Calen)
 	I2C_Write(RTC_WRITE_ADDRESS,SCMTSLA_Ack_Code);  //send address of RTC and write command
 	I2C_Write(RTC_SECONDS_ADDRESS,SCMTSLA_Ack_Code);  //move the pointer register to the first address (0x00)
 	I2C_Stop();
-	
+
 	I2C_start();
 	I2C_Write(RTC_READ_ADDRESS,SCMTSLA_Ack_Code);   //send address of RTC and read command
 	I2C_Read(&Seconds,ReceiveData_Ack_code);		//read seconds
@@ -47,7 +47,7 @@ void Read_RTC(u8* Time,u8* Calen)
 	I2C_Read(&Day,ReceiveData_Ack_code);			//read day
 	I2C_Read(&Date,ReceiveData_Ack_code);			//read date
 	I2C_Read(&Month,ReceiveData_Ack_code);			//read month
-	I2C_Read(&Year,ReceiveData_NAck_code);			//read year
+	I2C_Read(&Year,ReceiveData_NAck_code);			//read year   
 	I2C_Stop();
 	sprintf(Time,"%d%d:%d%d:%d%d",((Hours&0xf0)>>4),Hours&0x0f,((Minutes&0xf0)>>4),Minutes&0x0f,((Seconds&0xf0)>>4),Seconds&0x0f);
 	sprintf(Calen,"%d%d/%d%d/%d%d %s",((Date&0xf0)>>4),Date&0x0f,((Month&0xf0)>>4),Month&0x0f,((Year&0xf0)>>4),Year&0x0f,&days[Day]);
