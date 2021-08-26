27036d54d98b9dad7e8371962ae8a9036621b975
mmxa1 <89199209+mmxa1@users.noreply.github.com>
2021-08-26 19:56:12 +0200

    khraaaaaaaaaa

--- Atmega32 drivers/Atmega32 drivers/app/main.c
+++ Atmega32 drivers/Atmega32 drivers/app/main.c
@@ -16,18 +16,22 @@ int main(void)
 {
 	I2C_Init();
 	LCD_init();
-	u8 Time = 0;
-	I2C_start();
-	I2C_Write(0xD3,SCMTSLA_Ack_Code);
+	u8 Time = 0,i=0;
+	
+	
 	while (1)
 	{
+		I2C_start();
+		I2C_Write(0xD0,SCMTSLA_Ack_Code);
+		I2C_Write(0x05,SCMTSLA_Ack_Code);
+		I2C_Stop();
+		I2C_start();
+		I2C_Write(0xD1,SCMTSLA_Ack_Code);
+		I2C_Read(&Time,ReceiveData_NAck_code);
+		I2C_Stop();
 		_delay_ms(1000);
-		I2C_Read(&Time,ReceiveData_Ack_code);
-		disp_charXY(1,3,'p');
 		disp_intXY(1,1,Time);
 	}
-	
-	
-	
+				
 }
 
