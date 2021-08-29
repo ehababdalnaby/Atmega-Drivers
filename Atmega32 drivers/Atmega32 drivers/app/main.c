81be23030183dbe974632e321de892443ac4d3b4
ehababdalnaby <ehab_abdalnaby@outlook.com>
2021-08-30 00:28:47 +0200

    sim

--- Atmega32 drivers/Atmega32 drivers/app/main.c
+++ Atmega32 drivers/Atmega32 drivers/app/main.c
@@ -30,40 +30,9 @@ void servo_rotate(u32 angel)
 	timer_delay_us(20000-angel);
 }
 int main(void)
-{
-	pinDirection(PD5,OUTPUT);
-	u32 angel=0;
-	LCD_init();
-	
-	//Clock_APP();
+{	
+	Clock_APP();
 	//GPS_APP();
 	//GLCD_init();
-	Timer1_Init(FPWM);
-	ADC_init();
-	while(1)
-	{
-		angel= read_analog(ADC0);
-		angel=(angel*180)/1023UL;
-		disp_strXY(1,1,"angel = ");
-		disp_str("    ");
-		disp_intXY(1,9,angel);
-		servo_Angel(angel);
-	}
-	
-// 	Timer0_Init(NORMAL);
-// 	pinDirection(PD7,OUTPUT);
-// 	writePin(PD7,LOW);
-// 	LCD_init();
-// 	ADC_init();
-// 	
-// 	//tempsensorAPP();
-// 	u32 Pot_read=0;
-// 	while(1)
-// 	{
-// 		GLCD_Disp_customsize(64,64,smile);
-// 		_delay_ms(250);
-// 		GLCD_Disp_customsize(64,64,wink);
-// 		_delay_ms(250);
-// 	}
 }
 
